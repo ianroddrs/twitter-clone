a5a589079aac54bc5a70476bea9cf27e432096d4
ianroddrs <ianroddrs@gmail.com>
2026-01-25 22:37:09 -0300

    querys

---
+++ querys.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE twitter_clone
+DEFAULT CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+USE twitter_clone;
+CREATE TABLE usuarios(
+	 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    senha VARCHAR(32) NOT NULL
+);
