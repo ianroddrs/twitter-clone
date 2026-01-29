4a998bde4ff9a1bf3dc5c457a250a97fab22c9ba
ianroddrs <ianroddrs@gmail.com>
2026-01-29 19:18:06 -0300

    exibindo tweets de outros usuários

--- App/Models/Tweet.php
+++ App/Models/Tweet.php
@@ -40,13 +40,16 @@ class Tweet extends Model{
             SELECT t.id, t.id_usuario, u.nome, t.tweet, DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') as data
             FROM tweets as t
             LEFT JOIN usuarios as u ON (t.id_usuario = u.id)
-            WHERE t.id_usuario = ?
+            WHERE t.id_usuario = :id_usuario OR t.id_usuario IN (
+                SELECT id_usuario_seguindo FROM usuarios_seguidores
+                WHERE id_usuario = :id_usuario
+            )
             ORDER BY t.data desc
             ;
         ";
 
         $stmt = $this->db->prepare($query);
-        $stmt->bindValue(1, $this->__get('id_usuario'));
+        $stmt->bindValue(':id_usuario', $this->__get('id_usuario'));
         $stmt->execute();
 
         return $stmt->fetchALL(\PDO::FETCH_ASSOC);
